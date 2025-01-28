8cf2cfb456d1754301317609d8ecc689093ed815
Fernando <fernando14.05@hotmail.com>
2025-01-27 22:59:06 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,100 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p',' Escolha um número entre 1 e 10 ');
+}
+
+exibirMensagemInicial()
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`;
+        exibirTextoNaTela('h1', 'Parabéns, Você acertou!'); 
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random()* numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite) { 
+        listaDeNumerosSorteados = [];
+    }
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+
+
+
+
+/*let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do desafio';
+
+function verificarChute() {
+    console.log('O botão foi clicado!');
+}
+
+function amoJs() {
+    console.log('Eu amo Js');
+}
+
+function verificarCidade() {
+    let cidade = prompt();
+    alert(`Estive em ${cidade} e lembrei de você!`);
+}
+
+function somarDois() {
+    let num1 = parseInt(prompt('Informe um número'));
+    let num2 = parseInt(prompt('Informe outro número'));
+    let soma = num1 + num2;
+    alert(`A soma dos dois números informados são ${soma}`);
+}*/
+
+// IMC = PESO em kg / (altura(m) x altura(m))
+
+
+
+
+ 
